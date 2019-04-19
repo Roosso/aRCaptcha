6aec369701b49d07eda4e6e1de998f529d160000
alex Belov <roosso@ya.ru>
2019-04-19 18:43:49 +0300

    up to MIT license

--- stable v2.0.2/captcha.php
+++ stable v2.0.2/captcha.php
@@ -7,29 +7,6 @@
 |	Homepage: http://www.black-web.ru
 |	Copyright (c) 2007 alex Roosso.
 |	All rights reserved.
-|	===================================
-|
-|	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-|	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-|	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-|	FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-|	REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-|	INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-|	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-|	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-|	HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-|	STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-|	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-|	OF THE POSSIBILITY OF SUCH DAMAGE.
-|
-|	USAGE:
-|	This program is free software; you can redistribute it and/or
-|	modify it under the terms of the GNU General Public License
-|	as published by the Free Software Foundation; either version 2
-|	of the License, or (at your option) any later version.
-|
-|	More Info About The Licence At http://www.gnu.org/copyleft/gpl.html
-|	
 ========================================*/
 
 
